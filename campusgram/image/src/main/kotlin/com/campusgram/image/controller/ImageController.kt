0b6896054e6db637ee20b4c8feb18ed71192c01c
juhyeon1114 <juhyeon1114@gmail.com>
2025-04-02 22:50:26 +0900

    이미지 서버

---
+++ campusgram/image/src/main/kotlin/com/campusgram/image/controller/ImageController.kt
@@ -0,0 +1,34 @@
+package com.campusgram.image.controller
+
+import com.campusgram.image.service.ImageService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+import reactor.core.publisher.Flux
+
+@RestController
+@RequestMapping("/api/images")
+class ImageController(
+    private val imageService: ImageService
+) {
+
+    @GetMapping("/{imageId}")
+    suspend fun getImageById(
+        @PathVariable("imageId") imageId: Long
+    ): Image {
+        return imageService.findImageById(imageId)
+            ?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
+    }
+
+    @GetMapping
+    suspend fun getImagesByIds(
+        @RequestParam imageIds: List<Long>,
+    ): Flux<Image> {
+        return imageService.findImagesByIds(imageIds)
+    }
+
+}
